b174308a8999380df21b16bfaefc4bb74000e05d
Alkxs <72358387+Alkxs@users.noreply.github.com>
2023-05-09 15:18:25 +0200

    initial setup

---
+++ Beginner/reverseString/index-START.js
@@ -0,0 +1,33 @@
+/* CHALLENGE
+Given a string of text, write an algorithm that returns the text received in a reversed format. 
+E.g reverseString('algorithms') // should return 'smhtirogla'
+*/
+
+//Solutions
+
+// chaining built-in methods
+function reverseString(text) {
+   
+}
+// built in methods ES 6
+function reverseString(text) {
+   
+}
+// for loop way
+function reverseString(text) {
+    
+}
+// for of loop / ES 6 syntax
+function reverseString(text) {
+}
+// Recursion
+function reverseString(text) {
+}
+// Reduce
+function reverseString(text) {
+
+}
+
+
+
+module.exports = reverseString
---
+++ Beginner/vowelsCounter/index-START.js
@@ -0,0 +1,14 @@
+/* CHALLENGE
+Given a string of text, return the number of vowels found within the text
+e.g vowelsCounter('anehizxcv') // will return 3
+*/
+
+const vowels = ['a', 'e', 'i', 'o', 'u']
+
+//for of loop
+
+// regex
+
+
+
+module.exports = vowelsCounter;
