a5048645e99121084c5ad46ea4d4e55bb7e585b3
Alkxs <72358387+Alkxs@users.noreply.github.com>
2023-06-01 16:53:54 +0200

    Push/Insert data in Singly Linked List

--- Data Structures/Singly Linked List.js
+++ Data Structures/Singly Linked List.js
@@ -42,6 +42,18 @@ class LinkedList {
       this.tail = null
       this.length = 0
     }
+    push(val){
+      const newNode = new Node(val)
+      if(!this.head){
+        this.head = newNode
+        this.tail = this.head
+      } else {
+        this.tail.next = newNode
+        this.tail = newNode
+      }
+      this.length++
+      return this
+    }
 }
 
 
