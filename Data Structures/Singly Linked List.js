177fdfc384527859977ac638662adf7ddb4feb66
Alkxs <72358387+Alkxs@users.noreply.github.com>
2023-06-01 16:52:01 +0200

    Singly Linked List set up

---
+++ Data Structures/Singly Linked List.js
@@ -0,0 +1,47 @@
+// Definition
+
+- A data structure that contains a head, tail and length property.
+- Consists of nodes and each node has a value and a pointer to another node or null
+- A bunch of nodes pointing to the next one
+- Mainly supports 
+  * insertion 
+  * deletion
+  * Search
+
+Comparisons with arrays
+
+ Lists:
+- no indexes
+- connected via nodes with a next pointer
+- random access is not allowed
+
+Arrays:
+- indexed in order
+- insertion and deletion can be expensive
+- can quickly be accessed at a specific index
+
+// Main use and advantages
+
+You use a linked list if need easy insertion and deletion at the beginning or end of the list
+
+// Examples 
+
+- image viewer
+
+// Implementation
+
+class Node {
+    constructor(val) {
+      this.val = val
+      this.next = null
+    }
+}
+class LinkedList {
+    constructor(){
+      this.head = null
+      this.tail = null
+      this.length = 0
+    }
+}
+
+
