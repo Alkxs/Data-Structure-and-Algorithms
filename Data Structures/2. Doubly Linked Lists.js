e0dc558bbba29348a4d0ae0a0af0a01eebca217b
Alkxs <72358387+Alkxs@users.noreply.github.com>
2023-06-04 19:00:13 +0200

    Pop method Doubly Linked Lists added

--- Data Structures/2. Doubly Linked Lists.js
+++ Data Structures/2. Doubly Linked Lists.js
@@ -35,4 +35,22 @@ class DoublyLinkedList {
         this.length++
         return this
     }
+    // delete at the end of the list
+    pop(){
+        if (!this.head) {
+        return undefined;
+        }
+  
+        const poppedNode = this.tail;
+        if (this.length === 1) {
+          this.head = null;
+          this.tail = null;
+        } else {
+          this.tail = poppedNode.prev;
+          this.tail.next = null;
+          poppedNode.prev = null; 
+        }
+        this.length--;
+        return poppedNode.val;
+    }
 }
