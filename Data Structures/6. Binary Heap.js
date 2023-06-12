0c6e616409d28eeb6a13b9e76a5a2c2951542cf5
Alkxs <72358387+Alkxs@users.noreply.github.com>
2023-06-12 12:54:07 +0200

    Binary Heap Extract Max method added

--- Data Structures/6. Binary Heap.js
+++ Data Structures/6. Binary Heap.js
@@ -19,4 +19,75 @@ A specialized tree-based data structure similar to binary search tree, each node
     constructor() {
         this.array = [];
     }
+
+    insert(val) {
+        this.array.push(val);
+        this.bubbleUp();
+    }
+
+    bubbleUp() {
+        let idx = this.array.length - 1;
+        while (idx > 0) {
+            let element = this.array[idx];
+            let parentIdx = Math.floor((idx - 1) / 2);
+            let parent = this.array[parentIdx];
+            if (parent >= element) break;
+            this.array[idx] = parent;
+            this.array[parentIdx] = element;
+            idx = parentIdx;
+        }
+    }
+
+    extractMax() {
+        const max = this.array[0];
+        const end = this.array.pop();
+        if (this.array.length > 0) {
+            this.array[0] = end;
+            this.sinkDown();
+        }
+        return max;
+    }
+
+    sinkDown() {
+        let idx = 0;
+        const length = this.array.length;
+        const element = this.array[0];
+
+      while (true) {
+        let leftChildIdx = 2 * idx + 1;
+        let rightChildIdx = 2 * idx + 2;
+        let leftChild, rightChild;
+        let swap = null;
+
+        if (leftChildIdx < length) {
+            leftChild = this.array[leftChildIdx];
+            if (leftChild > element) {
+                swap = leftChildIdx;
+            }
+        }
+
+        if (rightChildIdx < length) {
+            rightChild = this.array[rightChildIdx];
+            if (
+                (swap === null && rightChild > element) ||
+                (swap !== null && rightChild > leftChild)
+            ) {
+                swap = rightChildIdx;
+            }
+        }
+
+        if (swap === null) break;
+        this.array[idx] = this.array[swap];
+        this.array[swap] = element;
+        idx = swap;
+        }
+      }
   }
+
+
+// BigO heaps
+
+* Insertion = O(log n)
+* Deletion = O(log n)
+* Peek = O(1)
+* Heapify = O(n)
