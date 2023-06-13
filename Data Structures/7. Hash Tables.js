23dec87b3c5c710e55571c293ee4df37b4999253
Alkxs <72358387+Alkxs@users.noreply.github.com>
2023-06-13 23:33:01 +0200

    Hash Tables initial setup

---
+++ Data Structures/7. Hash Tables.js
@@ -0,0 +1,13 @@
+// Definition
+
+Data structure that allows us to store key-value pairs. It's also known as Hash Map in some languages.
+
+The way it works is by using a hash function to compute an index into an array in which an element will be inserted or searched.
+
+// Operations
+
+* Insert
+* Delete 
+* Retrieve
+
+// Implementation
