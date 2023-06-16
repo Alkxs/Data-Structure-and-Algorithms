25a69ecdedd76bc8f41a39d57ff9cb039951a35f
Alkxs <72358387+Alkxs@users.noreply.github.com>
2023-06-16 22:32:37 +0200

    Graph Description added

---
+++ Data Structures/8. Graphs.js
@@ -0,0 +1,38 @@
+// Definition
+
+A non-linear data structure that is a collection of nodes and connections.
+All nodes are treated equally, there is no parent and no priority
+
+// Usage
+
+* Physical systems of roads, flights etc.
+* Social networks (linkedin connection)
+* Location / Mapping
+* recommendation engines
+
+// Terminology 
+
+* Vertex = a node
+* Edge = connection between nodes
+
+// Type of graphs
+
+* weighted / Unweighted = assigning values to edges (km in maps) or without 
+* Directed / Undirected = with directions (linkedin) or without(facebook)
+
+// Operations
+
+* Add Vertex
+* Add edges
+* Display vertex
+
+// Graph representation
+
+* Adjacency list: array of lists
+
+* Adjacency matrix : 2D array of size V x V (vertex), if edge the value is 1, otherwise is 0
+
+// Implementation
+
+// Big O
+
