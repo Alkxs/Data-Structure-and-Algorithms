ff8c399e47a7d17303cd3fb073e3bdecee5566d4
Alkxs <72358387+Alkxs@users.noreply.github.com>
2023-06-17 19:49:53 +0200

    Graph Traversal added

---
+++ Data Structures/9. Graph Traversal.js
@@ -0,0 +1,14 @@
+
+// Definition
+
+Graph traversal is the process of visiting (checking and/or updating) each vertex in a graph. 
+
+// Classification
+
+Look at previous sections as well
+
+* Depth-First Search (DFS): following a single path as far as possible
+
+* Breadth-First Search (BFS): all neighbor nodes at the present depth before moving on to nodes at the next depth level.
+
+Difference with trees is that there is not always a clear path.
