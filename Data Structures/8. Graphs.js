f99c187acd0473ac70cbaf8431252384a4e4d392
Alkxs <72358387+Alkxs@users.noreply.github.com>
2023-06-17 19:09:36 +0200

    Graphs added

--- Data Structures/8. Graphs.js
+++ Data Structures/8. Graphs.js
@@ -40,5 +40,44 @@ All nodes are treated equally, there is no parent and no priority
 * Bellman-Ford algorithm (for shortest path that handles negative weights)
 * Floyd-Warshall algorithm (for finding shortest paths between all pairs of nodes)
 
+// Implementation
+
+class Graph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+  
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+  }
+
+  addEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1].push(vertex2);
+    this.adjacencyList[vertex2].push(vertex1);
+  }
+
+  removeEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+      v => v !== vertex2
+    );
+    this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+      v => v !== vertex1
+    );
+  }
+
+  removeVertex(vertex) {
+    while(this.adjacencyList[vertex].length) {
+      const adjacentVertex = this.adjacencyList[vertex].pop();
+      this.removeEdge(vertex, adjacentVertex);
+    }
+    delete this.adjacencyList[vertex];
+  }
+
+  
+}
+
 // Big O
 
+* adding edge or vertex = O(1)
+* removing edge = 0(E)
+* removing vertex = O(V + N)
