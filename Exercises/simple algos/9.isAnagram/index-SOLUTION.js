907985567e7fbcb2c364d4db2ee3578caff8409f
Alkxs <72358387+Alkxs@users.noreply.github.com>
2023-06-26 19:40:49 +0200

    added memoization to factorial exercise

---
+++ Exercises/simple algos/9.isAnagram/index-SOLUTION.js
@@ -0,0 +1,53 @@
+// WITH SORT
+
+function isAnagram(stringA, stringB) {
+  let a = stringA.split('').sort().join('')
+  let b = stringB.split('').sort().join('')
+  return a === b
+}
+
+// DIRECT COMPARISON sanitized string
+
+function isAnagram(stringA, stringB) {
+  const sanitizeString = function (str) {
+    return str
+      .toLowerCase()
+      .replace(/[^a-z\d]/g, '')
+      .split('')
+      .sort()
+      .join('')
+  }
+
+  return sanitizeString(stringA) == sanitizeString(stringB)
+}
+
+// CHARACTER MAP COMPARISON
+
+function isAnagram(stringA, stringB) {
+  function createCharMap(text) {
+    let charMap = {}
+    for (let char of text) {
+      if (charMap.hasOwnProperty(char)) {
+        charMap[char]++
+      } else {
+        charMap[char] = 1
+      }
+    }
+    return charMap
+  }
+
+  if (stringA.length === stringB.length) {
+    let stringAMap = createCharMap(stringA)
+    let stringBMap = createCharMap(stringB)
+
+    for (let char in stringAMap) {
+      if (stringAMap[char] !== stringBMap[char]) {
+        return false
+      }
+    }
+
+    return true
+  } else {
+    return false
+  }
+}
---
+++ Exercises/simple algos/factorial/index-START.js
@@ -0,0 +1,11 @@
+/*
+Write a function that returns the factorial of 
+the provided integer(n). E.g
+    factorial(5) // should return 120
+*/
+
+function factorial() {}
+
+console.log(factorial(4, 5))
+
+module.exports = factorial
