44029c1ebc83d5a78f5382e3359bf0358470b229
Alkxs <72358387+Alkxs@users.noreply.github.com>
2023-07-03 22:52:41 +0200

    SLL pop exercise added

---
+++ Data Structures/DS exercises.js/2. SLL - pop exercise/SSL - pop exercise-solution.js
@@ -0,0 +1,34 @@
+// Solution
+
+class Node {
+  constructor(val) {
+    this.val = val
+    this.next = null
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null
+    this.tail = null
+    this.length = 0
+  }
+
+  pop() {
+    if (!this.head) return undefined
+    let current = this.head
+    let newTail = current
+    while (current.next) {
+      newTail = current
+      current = current.next
+    }
+    this.tail = newTail
+    this.tail.next = null
+    this.length--
+    if (this.length === 0) {
+      this.head = null
+      this.tail = null
+    }
+    return current
+  }
+}
